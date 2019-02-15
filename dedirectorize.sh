a9cc8ecbfc5453c6c4f71daeb915fc28bea5dc8f
Giulio Starace <giulio.starace@gmail.com>
2019-02-15 11:08:16 +0000

    better spacing for legibility

--- dedirectorize.sh
+++ dedirectorize.sh
@@ -2,16 +2,22 @@
 # thank you to
 # https://stackoverflow.com/questions/1527049/how-can-i-join-elements-of-an-array-in-bash
 function join_by { local IFS="$1"; shift; echo "$*"; }
+
 # give name to dedirectorized directory
 dedirectorizeddirectory="dedirectorized/"
+
 IFS=$'\n' # use only line breaks as delimiters
+
 # iterate over all files in all subdirectories of current directory
 # ignoring .DS_Store, Icon?
 for i in $( find * -type f ! -name '*.DS_Store' ! -name 'Icon?' ! -name 'dedirectorize.sh'); do
-  IFS='/.' # split item via forward slashes (levels of query)
-  read -r -a directoryarray <<< "$i" # read the split items into an array
+  # split item via forward slashes (levels of query)
+  IFS='/.'
+  # read the split items into an array
+  read -r -a directoryarray <<< "$i"
   finalfilename=$(join_by . ${directoryarray[@]})
-  IFS=$'\n' # resetting linebreaks as only delimiters
+  # resetting linebreaks as only delimiters
+  IFS=$'\n'
   # prepare location for cp command so that files are renamed in process.
   finallocation="${dedirectorizeddirectory}${finalfilename}"
   # create directory into which to copy files
